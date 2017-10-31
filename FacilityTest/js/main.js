15dde453c8f8b0694530a266e89d3ce761683775
Bodicj <Nitron20>
2017-10-31 09:31:25 +0200

    Add mobile head menu

--- FacilityTest/js/main.js
+++ FacilityTest/js/main.js
@@ -7,12 +7,32 @@ $(document).ready(function () {
 
         var target = this.hash,
         $target = $(target);
-        console.log($(target));
 
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
+            'scrollTop': $target.offset().top-80
         }, 900, 'swing', function () {
-            window.location.hash = target;
+            window.location.hash = target - 80;
         });
+    });
+    $('.hamburger-menu').click(function (e){
+        e.preventDefault();
+        if ($('header').css('height') == '80px'){
+            $('header').css('height', '2075px');
+            $('nav').css('display', 'flex');
+            $('.hamburger-menu').css('display', 'none');
+            $('.close').css('display', 'block');
+        }
+        else{
+        }
+    });
+    $('.close').click(function (e) {
+        e.preventDefault();
+        console.log($('header').css('height'));
+        if ($('header').css('height') != '80px') {
+            $('header').css('height', '80px');
+            $('nav').css('display', 'none');
+            $('.hamburger-menu').css('display', 'block');
+            $('.close').css('display', 'none');
+        }
     });
 });
